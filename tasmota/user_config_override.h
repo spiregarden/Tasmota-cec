5e1750bb60e571eb72751b1b8fd8cef0ffeabbf1
spiregarden <79320948+spiregarden@users.noreply.github.com>
2025-11-22 12:26:30 +0200

    Add user_config_override.h for HDMI-CEC support

---
+++ tasmota/user_config_override.h
@@ -0,0 +1,10 @@
+#ifndef _USER_CONFIG_OVERRIDE_H_
+#define _USER_CONFIG_OVERRIDE_H_
+
+// --- HDMI-CEC support ---
+#define USE_HDMI_CEC
+#ifndef USE_I2C
+#define USE_I2C
+#endif
+
+#endif  // _USER_CONFIG_OVERRIDE_H_
